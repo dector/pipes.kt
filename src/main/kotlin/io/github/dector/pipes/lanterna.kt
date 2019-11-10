db80555fac530760e8538e02ab7ca58438ec85fd
Denys M <dector9@gmail.com>
2019-11-10 13:14:18 +0400

    Move lanterna wrapper into separate file.

---
+++ src/main/kotlin/io/github/dector/pipes/lanterna.kt
@@ -0,0 +1,36 @@
+package io.github.dector.pipes
+
+import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.terminal.Terminal
+
+fun wrap(term: Terminal) =
+    object : Term {
+
+        override val width = term.terminalSize.columns
+        override val height = term.terminalSize.rows
+
+        override fun hideCursor() {
+            term.setCursorVisible(false)
+        }
+
+        override fun setTextColor(color: Color) {
+            term.setForegroundColor(color.asLanternaColor())
+        }
+
+        override fun printAt(x: Int, y: Int, c: Char) {
+            term.setCursorPosition(x, y)
+            term.putCharacter(c)
+        }
+
+        override fun flush() {
+            term.flush()
+        }
+    }
+
+fun Color.asLanternaColor() = when (this) {
+    Color.Blue -> TextColor.ANSI.BLUE
+    Color.Red -> TextColor.ANSI.RED
+    Color.Yellow -> TextColor.ANSI.YELLOW
+    Color.Magenta -> TextColor.ANSI.MAGENTA
+    Color.Green -> TextColor.ANSI.GREEN
+}
--- src/main/kotlin/io/github/dector/pipes/main.kt
+++ src/main/kotlin/io/github/dector/pipes/main.kt
@@ -1,8 +1,6 @@
 package io.github.dector.pipes
 
-import com.googlecode.lanterna.TextColor
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
-import com.googlecode.lanterna.terminal.Terminal
 
 fun main() {
     launchApp()
@@ -13,36 +11,4 @@ fun launchApp() {
         .createTerminal()
         .let(::wrap)
     Application(terminal).run()
-}
-
-private fun wrap(term: Terminal) =
-    object : Term {
-
-        override val width = term.terminalSize.columns
-        override val height = term.terminalSize.rows
-
-        override fun hideCursor() {
-            term.setCursorVisible(false)
-        }
-
-        override fun setTextColor(color: Color) {
-            term.setForegroundColor(color.asLanternaColor())
-        }
-
-        override fun printAt(x: Int, y: Int, c: Char) {
-            term.setCursorPosition(x, y)
-            term.putCharacter(c)
-        }
-
-        override fun flush() {
-            term.flush()
-        }
-    }
-
-fun Color.asLanternaColor() = when (this) {
-    Color.Blue -> TextColor.ANSI.BLUE
-    Color.Red -> TextColor.ANSI.RED
-    Color.Yellow -> TextColor.ANSI.YELLOW
-    Color.Magenta -> TextColor.ANSI.MAGENTA
-    Color.Green -> TextColor.ANSI.GREEN
 }
